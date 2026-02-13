ff52b8e23aed0fe42912d41b451a63ef1cb54383
BHFResearch <hrdmny80@gmail.com>
2026-02-13 23:52:07 +0530

    sitemap update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://research.betterhealthfacts.com',
-  output: 'static'
+  output: 'static',
+  integrations: [sitemap()],
 });
