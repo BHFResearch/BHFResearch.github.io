c92385a438efd66f9468f73066b34ed301eaabf9
BHFResearch <hrdmny80@gmail.com>
2026-02-13 23:09:11 +0530

    making changes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,1 +1,10 @@
+---
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://research.betterhealthfacts.com',
+  integrations: [sitemap()],
+});
+---
+
 npm install @astrojs/sitemap
